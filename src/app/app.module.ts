b1a6c5988df752b7aebb3d78f81a1c6a9aa1f676
preetsamra <preetsamra@users.noreply.github.com>
2022-11-16 15:33:26 -0500

    5 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
-import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core'
-import { NativeScriptModule } from '@nativescript/angular'
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { NativeScriptModule } from '@nativescript/angular';
 
-import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component'
-import { ItemsComponent } from './item/items.component'
-import { ItemDetailComponent } from './item/item-detail.component'
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ItemsComponent } from './item/items.component';
+import { ItemDetailComponent } from './item/item-detail.component';
+import { NativeScriptUIListViewModule } from 'nativescript-ui-listview/angular';
 
 @NgModule({
   bootstrap: [AppComponent],
-  imports: [NativeScriptModule, AppRoutingModule],
+  imports: [NativeScriptModule, AppRoutingModule, NativeScriptUIListViewModule],
   declarations: [AppComponent, ItemsComponent, ItemDetailComponent],
   providers: [],
   schemas: [NO_ERRORS_SCHEMA],
--- src/app/item/items.component.ts
+++ src/app/item/items.component.ts
@@ -1,18 +1,22 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit } from '@angular/core';
 
-import { Item } from './item'
-import { ItemService } from './item.service'
+import { Item } from './item';
+import { ItemService } from './item.service';
 
 @Component({
   selector: 'ns-items',
   templateUrl: './items.component.html',
 })
 export class ItemsComponent implements OnInit {
-  items: Array<Item>
+  items: Array<Item>;
+  myGroupingFunc: any;
 
   constructor(private itemService: ItemService) {}
 
   ngOnInit(): void {
-    this.items = this.itemService.getItems()
+    this.items = this.itemService.getItems();
+    this.myGroupingFunc = (item: Item) => {
+      return item.role;
+    };
   }
 }
